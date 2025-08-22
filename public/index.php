25577be18b90bcda9589f96ca0113d6530a29b06
Eduarda <eduarda_tomaz@estudante.sesisenai.org.br>
2025-08-22 12:27:58 -0300

    add

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php include __DIR__.'/partials/header.php'; ?>
+<?php include __DIR__.'partials/header.php'; ?>
 <div class="grid cols-2">
   <div class="card">
     <h2>Sobre</h2>
@@ -6,11 +6,11 @@
   <div class="card">
     <h2>Atalhos</h2>
     <div class="actions">
-      <a class="btn primary" href="/partidas/create.php">Nova Partida</a>
-      <a class="btn" href="/times/">Listar Times</a>
-      <a class="btn" href="/times/create.php">Novo Time</a>
-      <a class="btn" href="/jogadores/create.php">Novo Jogador</a>
+      <a class="btn primary" href="partidas/create.php">Nova Partida</a>
+      <a class="btn" href="times">Listar Times</a>
+      <a class="btn" href="times/create.php">Novo Time</a>
+      <a class="btn" href="jogadores/create.php">Novo Jogador</a>
     </div>
   </div>
 </div>
-<?php include __DIR__.'/partials/footer.php'; ?>
+<?php include __DIR__.'partials/footer.php'; ?>
