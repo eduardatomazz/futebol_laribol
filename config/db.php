d90dbaae6c5bbf584b5eebf4fce908b41a6068ec
Braian Escher <braian.escher@gmail.com>
2025-08-22 11:27:53 -0300

    UPDATE- Create.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "padaria_paodango";
+$dbname = "futebol_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
